9852738fadaf63f1b00d7991db664d84d247cd09
manu <manu@example.com>
2021-10-21 17:36:24 +0530

    final report

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,38 @@
 class ArticlesController < ActionController::Base
-   def index
-       @articles=Article.all
-   end
    def show
        @article=Article.find(params[:id])
    end
+   def index
+       @articles=Article.all
+   end
+   
    def new
        @article=Article.new
    end
-   def create
-    @article=Article.new(params.require(:article).permit(:title,:description))
-    if @article.save
-        redirect_to @article
-    else
-        render 'new'
-    end
-   end
    def edit
-   
+     byebug
+     @article=Article.find(params[:id]) 
    end
-
+   def create
+       @article=Article.new(params.require(:article).permit(:title,:description))
+           if @article.save
+               redirect_to @article
+           else
+               render 'new'
+           end
+   end
+   def update
+       @article=Article.find(params[:id])
+       if @article.update(params.require(:article).permit(:title,:description))
+          flash[:notice]="Article was updated"
+          redirect_to @article
+       else
+          render 'edit'
+       end
+   end
+   def destroy
+      @article=Article.find(params[:id]) 
+      @article.destroy
+      redirect_to articles_path
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  root 'articles#index'
-  get 'articles/:id',to: 'articles#show'
-  get '/articles/', to: 'articles#index'
-  get '/article/', to: 'articles#new'
-  post '/article', to: 'articles#create'
-  
-  resource :articles
+  root 'articles#home'
+  get 'about',to: 'articles#about'
+  delete '/article/:id',to: 'articles#destroy'
+  resources :articles
 end
